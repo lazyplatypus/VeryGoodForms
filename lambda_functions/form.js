ce4243744f30ccbadc5b96a1d8fd736200521ad4
emsesc <emseschen@gmail.com>
2021-01-15 15:03:57 -0500

    rollback changes

--- lambda_functions/form.js
+++ lambda_functions/form.js
@@ -1,8 +1,9 @@
-// ./lambda_functions/pokemon.js
+// ./lambda_functions/form.js
 
 const MongoClient = require("mongodb").MongoClient;
 
 const MONGODB_URI = process.env.MONGODB_URI;
+// Place this environment variable in Netlify
 const DB_NAME = 'formboiz';
 
 let cachedDb = null;
@@ -22,63 +23,53 @@ const connectToDatabase = async (uri) => {
 };
 
 const queryDatabase = async (db) => {
-    const surveys = await db.collection("surveys").find({}).toArray();
-  
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(surveys),
-    };
+  const surveys = await db.collection("surveys").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(surveys),
   };
-  
-  module.exports.handler = async (event, context) => {
-    // otherwise the connection will never complete, since
-    // we keep the DB connection alive
-    context.callbackWaitsForEmptyEventLoop = false;
-  
-    const db = await connectToDatabase(MONGODB_URI);
-    return queryDatabase(db);
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
+};
+
+const pushToDatabase = async (db, data) => {
+  const surveys = {
+    question: data.questions,
+    hash: data.hash,
   };
 
-  const pushToDatabase = async (db, data, collection) => {
-    const collect;
-    if (collection == "surveys") {
-        collect = {
-            content: data.questions,
-            hash: data.hash,
-          };
-    }
-    else if (collection == "responses") {
-        collect = {
-            content: data.responses,
-            hash: data.hash,
-          };    
-        }
+  if (surveys.question && surveys.hash) {
+    await db.collection("surveys").insertMany([data]);
+    return { statusCode: 201 };
+  } else {
+    return { statusCode: 422 };
+  }
+};
 
-  
-    if (collect.content && collect.hash) {
-      await db.collection(collection).insertMany([data]);
-      return { statusCode: 201 };
-    } else {
-      return { statusCode: 422 };
-    }
-  };
-  
-  module.exports.handler = async (event, context) => {
-    // otherwise the connection will never complete, since
-    // we keep the DB connection alive
-    context.callbackWaitsForEmptyEventLoop = false;
-  
-    const db = await connectToDatabase(MONGODB_URI);
-  
-    switch (event.httpMethod) {
-      case "GET":
-        return queryDatabase(db);
-      case "POST":
-        return pushToDatabase(db, JSON.parse(event.body), "surveys");
-      default:
-        return { statusCode: 400 };
-    }
-  };
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+
+  switch (event.httpMethod) {
+    case "GET":
+      return queryDatabase(db);
+    case "POST":
+      return pushToDatabase(db, JSON.parse(event.body));
+    default:
+      return { statusCode: 400 };
+  }
+};
