8072f1acb2732160c6fe86ff121eeb4bc4768dfc
emsesc <emseschen@gmail.com>
2021-01-16 10:31:53 -0500

    move cachedDb

--- lambda_functions/helpers.js
+++ lambda_functions/helpers.js
@@ -1,6 +1,6 @@
-let cachedDb = null;
-
 const connectToDatabase = async (uri) => {
+  let cachedDb = null;
+
   // we can cache the access to our database to speed things up a bit
   // (this is the only thing that is safe to cache here)
   if (cachedDb) return cachedDb;
